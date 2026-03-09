f2cc1542726681746883624c2935067faee6a199
Kaue557 <kauelima557@gmail.com>
2026-03-08 23:27:35 -0300

    exercicios - arvore binaria

--- src/arvore_int/Arvore.java
+++ src/arvore_int/Arvore.java
@@ -53,6 +53,124 @@ public class Arvore {
         return p;
     }
 
+    /*
+    -------------------------------------------------------------
+    1. Escreva uma funçao recursiva para contar:
+        (a) o número total de nós;
+        (b) o número de folhas;
+        (c) o número de nós internos (com pelo menos um filho).
+    */
+    public int contaNo(No atual){
+        if(atual == null) return 0;
+
+        else{
+            int cont = 0;
+            return cont = 1 + contaNo(atual.getEsquerda()) + contaNo(atual.getDireita()); // atual +  lado esq + lado dir
+        }
+    }
+
+    public int contaFolha(No atual){
+        if(atual == null) return 0; // se no for nulo
+
+        else if(atual.getEsquerda() == null && atual.getDireita() == null){ // se no for folha
+            return 1;
+        }
+        else{
+            return contaFolha(atual.getEsquerda()) + contaFolha(atual.getDireita()); // se no for interno
+        }
+    }
+
+    public int contaInterno(No atual){
+        if(atual == null) return 0; // se no for nulo
+
+        else if(atual.getEsquerda() != null || atual.getDireita() != null){ // se no for interno
+            return 1 + contaInterno(atual.getEsquerda()) + contaInterno(atual.getDireita());
+        }
+        else{
+            return contaInterno(atual.getEsquerda()) + contaInterno(atual.getDireita()); // se no for folha
+        }
+    }
+    /*
+    -------------------------------------------------------------
+    2. Escreva um metodo que devolve o clone da arvore
+    */
+
+    public No cloneArvore(No atual){
+        if(atual == null){
+            return null;
+        }
+
+        No novo = new No(atual.getValor());
+
+        novo.setEsquerda(cloneArvore(atual.getEsquerda()));
+        novo.setDireita(cloneArvore(atual.getDireita()));
+
+        return novo;
+    }
+
+    /*
+    -------------------------------------------------------------
+    3. Escreva um metodo que devolve o espelho da árvore
+    (o que está a esquerda da árvore original, estara a direita no espelho e vice-versa
+    */
+
+    public void inverte(No atual){
+        if(atual == null) return;
+
+        No temp = atual.getEsquerda();
+        atual.setEsquerda(atual.getDireita());
+        atual.setDireita(temp);
+
+        inverte(atual.getEsquerda());
+        inverte(atual.getDireita());
+    }
+
+    /*
+    -------------------------------------------------------------
+    4. Dada uma arvore binaria qualquer, implemente uma funçao que determine se ela é uma BST valida.
+    */
+
+    public boolean ehBST(No atual, int min, int max){
+        if(atual == null){
+            return true;
+        }
+
+        if(atual.getValor() <= min || atual.getValor() >= max){
+            return false;
+        }
+
+        return ehBST(atual.getEsquerda(), min, atual.getValor()) &&
+                ehBST(atual.getDireita(), atual.getValor(), max);
+    }
+
+    public boolean ehBST(){
+        return ehBST(this.raiz, Integer.MIN_VALUE, Integer.MAX_VALUE);
+    }
+    /*
+    -------------------------------------------------------------
+    5. Escreva um metodo que remova todas as folhas da arvore.
+    */
+    public No removeFolhas(No atual){
+
+        if(atual == null){
+            return null;
+        }
+
+        // se for folha
+        if(atual.getEsquerda() == null && atual.getDireita() == null){
+            if(atual == raiz){
+                raiz = null;
+            }
+            return null;
+        }
+
+        atual.setEsquerda(removeFolhas(atual.getEsquerda()));
+        atual.setDireita(removeFolhas(atual.getDireita()));
+        return atual;
+    }
+
+
+
     // Percursos -------------------------------------
 
     public void emOrdem(No atual) { // esquerda - raiz - direita
--- src/arvore_int/Main.java
+++ src/arvore_int/Main.java
@@ -11,7 +11,9 @@ public class Main {
         arvore.adiciona(45);
         arvore.adiciona(51);
         arvore.adiciona(19);
+        System.out.println();
 
+        /*
         System.out.println("Em ordem:");
         arvore.emOrdem(arvore.getRaiz());
 
@@ -20,5 +22,51 @@ public class Main {
 
         System.out.println("\nPos-ordem:");
         arvore.posOrdem(arvore.getRaiz());
+        */
+
+        // EXERCICIO 1
+        System.out.println("EXERCICIO 1");
+        int total_nos = arvore.contaNo(arvore.getRaiz());
+        System.out.println("total de nos: " + total_nos);
+
+        int total_folhas = arvore.contaFolha(arvore.getRaiz());
+        System.out.println("total de folhas: " + total_folhas);
+
+        int total_internos = arvore.contaInterno(arvore.getRaiz());
+        System.out.println("total de Nos internos: " + total_internos);
+        System.out.println();
+
+        // EXERCICIO 2
+        System.out.println("EXERCICIO 2");
+        No raizClone = arvore.cloneArvore(arvore.getRaiz());
+
+        System.out.println("Arvore original:");
+        arvore.emOrdem(arvore.getRaiz());
+
+        System.out.println();
+
+        System.out.println("Arvore clone:");
+        arvore.emOrdem(raizClone);
+        System.out.println();
+
+        // EXERCICIO 3
+        System.out.println("EXERCICIO 3");
+        arvore.inverte(arvore.getRaiz());
+        arvore.emOrdem(arvore.getRaiz());
+        System.out.println();
+
+        // EXERCICIO 4
+        System.out.println("EXERCICIO 4");
+        if(arvore.ehBST()){
+            System.out.println("A arvore é uma BST");
+        }else{
+            System.out.println("A arvore NÃO é uma BST");
+        }
+        System.out.println();
+
+        // EXERCICIO 5
+        System.out.println("EXERCICIO 5");
+        arvore.removeFolhas(arvore.getRaiz());
+        arvore.emOrdem(arvore.getRaiz());
     }
 }
